b46cb91d85115bd5dd7fa6e4ec80737bb56fb65d
Marcelo Michels <contato@marcelomichels.com>
2019-03-14 16:13:53 -0300

    implementado recurso do s3 de upload de imagem

---
+++ s3.js
@@ -0,0 +1,28 @@
+
+var AWS = require('aws-sdk');
+const uuidv4 = require('uuid/v4');
+
+AWS.config.update({ region: 'us-east-1' });
+
+module.exports = class S3 {
+
+    static async uploadBase64(bucket, base64, contentType) {
+
+        var s3Bucket = new AWS.S3({ params: { 'Bucket': bucket } });
+
+        let buffer = new Buffer(base64.replace(/^data:image\/\w+;base64,/, ""), 'base64')
+
+        let name = uuidv4();
+        name = name.replace(/-/g, '');
+        var params = {
+            'Key': name,
+            'Body': buffer,
+            'ContentEncoding': 'base64',
+            'ContentType': contentType,
+            'ACL': 'public-read',
+        };
+
+        await s3Bucket.putObject(params).promise();
+        return name;
+    }
+} 
